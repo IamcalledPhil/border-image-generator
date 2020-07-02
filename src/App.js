08780734feafe984586db932252208c10a01629b
iamcalledphil <philippetercohn@gmail.com>
2020-07-02 19:38:07 +0200

    started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+const BrushStroke1 = (props) => {
+  console.log(props.translateX);
+  const move=`translate(${props.translateX},${props.translateY})`;
+
+  let path = <path d="M0,13S29.15,0,36.79,0,89.62,13,89.62,13s-29,3-47.32,3C27,16,0,13,0,13Z" fill="#262626" transform={move}/>;
+  return (path);
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <section className="canvas-container">
+        <svg >
+          <BrushStroke1 translateX="0" translateY="0"/>
+          <BrushStroke1 translateX="100" translateY="100"/>
+        </svg>
+      </section>
     </div>
   );
 }
