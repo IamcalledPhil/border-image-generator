014b3d6ac49f176a40f8bb1be0d984c2c9276e1f
iamcalledphil <philippetercohn@gmail.com>
2020-07-26 13:48:52 +0200

    card layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import './App.scss';
+import './styles/App.scss';
 import './components/ImageCreator';
 import ImageCreator from './components/ImageCreator';
 import BorderImageSettings from './components/border-image-settings/BorderImageSettings';
 import Header from './components/Header';
+import Output from './components/Output';
 
 class App extends React.Component {
   
@@ -13,7 +14,9 @@ class App extends React.Component {
         <Header/>
         <div className="content">
           <ImageCreator/>
+          <Output/>
           <BorderImageSettings/>
+
         </div>
       </div>
     );
---
+++ src/components/Output.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+import { useSelector } from "react-redux";
+
+import '../styles/output.scss';
+
+const Output = () => {
+  const svgURI = useSelector(state => state.imageCreator.svgURI);
+  const imageColor = useSelector(state => state.imageCreator.imageColor);
+
+  const unit = useSelector(state => state.borderImage.unit);
+  const borderRepeat = useSelector(state => state.borderImage.borderImageRepeat);
+  const borderWidth = useSelector(state => state.borderImage.borderWidth);
+  const borderOutset = useSelector(state => state.borderImage.borderImageOutset);
+  const borderSlice = useSelector(state => state.borderImage.borderImageSlice);
+  const demoDivStyle = {
+    "borderStyle": "solid",
+    "borderImageWidth": `${borderWidth.top}${unit} ${borderWidth.right}${unit} ${borderWidth.bottom}${unit} ${borderWidth.left}${unit}`,
+    "borderImageSource": `url(${svgURI})`,
+    "borderImageOutset": `${borderOutset.top}${unit} ${borderOutset.right}${unit} ${borderOutset.bottom}${unit} ${borderOutset.left}${unit}`,
+    "borderImageSlice": `${borderSlice.top} ${borderSlice.right} ${borderSlice.bottom} ${borderSlice.left}`,
+    "borderImageRepeat": `${borderRepeat} ${borderRepeat}`,
+    // should be able to set background color too in case don't want outset
+    "backgroundColor": `${imageColor}`
+  }
+
+  return (
+      <div className="output">
+        <h1>Result:</h1>
+        <div className="demo-div" style={demoDivStyle}></div>
+        <a href={svgURI}  download>Download image</a>
+
+        <h3>CSS code</h3>
+        <div className="border-settings-text">
+
+          <code>
+            border-image-width: {demoDivStyle.borderImageWidth};<br/>
+            border-image-slice: {demoDivStyle.borderImageSlice};<br/>
+            border-image-outset: {demoDivStyle.borderImageOutset};<br/>
+            border-image-repeat: {demoDivStyle.borderImageRepeat};<br/>
+            border-style: {demoDivStyle.borderStyle};<br/>
+            background-color: {demoDivStyle.backgroundColor};<br/>
+          </code>
+        </div>
+      </div>
+      
+  )
+};
+
+export default Output;
